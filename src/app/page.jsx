ae94a39858f80978efa205dcd9592a3e728061c8
aryakarthi <aryaperumal.m@gmail.com>
2023-12-28 19:32:51 +0530

    Completed initial css and layout setup

---
+++ src/app/page.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./page.module.css";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/menu/Menu";
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
