dd41e5b49a4139de46ebc663382ff97ddd28fd32
aryakarthi <aryaperumal.m@gmail.com>
2023-12-29 23:29:40 +0530

    Completed about page ui

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -2,6 +2,8 @@
 import React, { useState } from "react";
 import styles from "./authLinks.module.css";
 import Link from "next/link";
+import { HiMenu } from "react-icons/hi";
+
 const AuthLinks = () => {
   const [open, setOpen] = useState();
   const status = "unauthenticated";
@@ -14,12 +16,28 @@ const AuthLinks = () => {
           </Link>
         </>
       ) : (
-        <>
+        <div className={styles.authLinks}>
           <Link href="/write" className={styles.link}>
             Write
           </Link>
           <span className={styles.link}>Logout</span>
-        </>
+        </div>
+      )}
+      <HiMenu className={styles.hamburger} onClick={() => setOpen(!open)} />
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Home</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          {status === "unauthenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <div className={styles.logout}>Logout</div>
+            </>
+          )}
+        </div>
       )}
     </div>
   );
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 import styles from "./featured.module.css";
+import Image from "next/image";
 const Featured = () => {
-  return <div>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        Hey!
+        <br />
+        I'm <span className={styles.authorName}>aryakarthi</span>.
+      </h1>
+      <p className={styles.authorMsg}>
+        Discover my stories and creative ideas..!
+      </p>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/featured_img.jpg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Welcome to Blogtopia</h1>
+          <p className={styles.postDesc}>
+            We are passionate about sharing our knowledge and experiences with
+            the world. Our goal is to create a platform where we can connect
+            with people who share our interests and inspire them to learn and
+            grow.
+            <br /><br />
+            We believe that everyone has something valuable to contribute, and
+            we want to help you share your voice with the world. Join us on our
+            journey and let’s explore the world together!
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
